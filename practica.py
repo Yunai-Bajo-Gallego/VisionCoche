7e16e2c4e9a116f618e6b906bb9b4ec6f4e2c1e8
Yunai-Bajo-Gallego <yunaibg@hotmail.com>
2018-03-13 18:09:26 +0100

    pillar3puntos

--- practica.py
+++ practica.py
@@ -12,13 +12,12 @@ for x in range(ìndex_training_ini+1):
 
     img = cv2.imread("training/frontal_"+str(x+1)+".jpg",0)
     # Initiate ORB detector
-    orb = cv2.ORB_create((int nfeatures=3, float scaleFactor=1.0f, int nlevels=1)
+    orb = cv2.ORB_create(nfeatures=3,scaleFactor=1.0,nlevels=1)
     # find the keypoints with ORB
     kp = orb.detect(img,None)
     # compute the descriptors with ORB
     kp, des = orb.compute(img, kp)
-    for y in des:
-        print(des[y])
+
     img2 = cv2.drawKeypoints(img,kp,None,color=(0,255,0), flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
     plt.imshow(img2),plt.show()
 # draw only keypoints location,not size and orientation
