ea1aa63ea73d7c46495cced1dbfa4cda213d470c
Yunai-Bajo-Gallego <yunaibg@hotmail.com>
2018-03-19 15:31:37 +0100

    Empezar 4ª hoja
    3 primeras controladas, buscar el centro de la imagen y guardar vector

--- practica.py
+++ practica.py
@@ -85,7 +85,17 @@ FLANN_INDEX_KDTREE = 1
 index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
 search_params = dict(checks=50)   # or pass empty dictionary
 flann = cv2.FlannBasedMatcher(index_params,search_params) #método add
+
+flann.add(des1)
+flann.add(des2)
+
 matches = flann.knnMatch(queryDescriptors=des1,trainDescriptors=des2,k=2)
+i=0
+for r in matches:
+    for m in r:
+        print(i," ","Resultado - distancia:",m.distance," img: ",m.imgIdx," queryIdx: ", m.queryIdx," trainIdx:",m.trainIdx)
+        i=i+1
+
 # Need to draw only good matches, so create a mask
 matchesMask = [[0,0] for i in range(len(matches))]
 # ratio test as per Lowe's paper
